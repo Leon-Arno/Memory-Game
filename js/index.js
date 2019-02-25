65ea9d19aaee0492bc711f2137bd9436689e4e5b
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-02-25 13:15:52 +0100

    added match and disable functios

--- js/index.js
+++ js/index.js
@@ -2,10 +2,35 @@
 
 const cards = document.querySelectorAll('.card');
 
+let flippedCard = false;
+let firstCard;
+let secondCard;
+
 //* creates a flip class
 function cardFlip() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  flippedCard = false;
 }
+
+function checkCardMatch() {
+  if (firstCard.dataset.technologies === secondCard.dataset.technologies) {
+    disableCards();
+    return;
+  }
+}
+
+const disableCards = () => {
+  firstCard.removeEventListener('click', cardFlip);
+  secondCard.removeEventListener('click', cardFlip);
+};
 
 //* Loops through each card to attach eventListener
 cards.forEach(card => card.addEventListener('click', cardFlip));
