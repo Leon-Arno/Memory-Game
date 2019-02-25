a5c20be48edbe1d07fb71703c41577c11b02c1c6
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-02-25 15:20:44 +0100

    Added resetBoard function

--- js/index.js
+++ js/index.js
@@ -5,9 +5,13 @@ const cards = document.querySelectorAll('.card');
 let flippedCard = false;
 let firstCard;
 let secondCard;
+let lockCard = false;
 
 //* creates a flip class
-function cardFlip() {
+let cardFlip = function() {
+  // ? prevents triple flipping.
+  if (lockCard) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
 
   if (!flippedCard) {
@@ -15,21 +19,35 @@ function cardFlip() {
     firstCard = this;
     return;
   }
-
   secondCard = this;
-  flippedCard = false;
-}
 
-function checkCardMatch() {
-  if (firstCard.dataset.technologies === secondCard.dataset.technologies) {
-    disableCards();
-    return;
-  }
-}
+  checkCardMatch();
+};
+
+const checkCardMatch = () => {
+  let matched =
+    firstCard.dataset.technologies === secondCard.dataset.technologies;
+  matched ? disableCards() : unflipCards();
+};
 
 const disableCards = () => {
   firstCard.removeEventListener('click', cardFlip);
   secondCard.removeEventListener('click', cardFlip);
+  resetBoard();
+};
+
+const unflipCards = () => {
+  lockCard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1200);
+};
+
+const resetBoard = () => {
+  [flippedCard, lockCard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 };
 
 //* Loops through each card to attach eventListener
